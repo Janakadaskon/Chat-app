d3f7e44d2fc22a0be91b11b5f5ba290788744399
Janaka Daskon <janakadaskon@gmail.com>
2023-11-29 21:37:02 +0530

    create chatsPage

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,1 +1,15 @@
 import{ PrettyChatWindow } from 'react-chat-engine-pretty'
+const ChatsPage = () => {
+    return(
+        <div style={{height: '100vh'}}>
+            <PrettyChatWindow 
+                projectId=''
+                username=''
+                secret=''
+                style={{height: '100%'}}
+            />
+        </div>
+    )
+}
+
+export default ChatsPage
